4bb06096bebfdd552ba81719138355eee042462d
Boris Napolsky <xidy@ya.ru>
2023-09-04 10:41:28 +0300

    Домашнее задание для семинара 6

---
+++ DZ_5/Zadacha_41/Program.cs
@@ -0,0 +1,85 @@
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string[] input = Console.ReadLine().Split(' ');
+
+int[] numbers = new int[input.Length];
+
+for (int i = 0; i < input.Length; i++)
+{
+    bool success = Int32.TryParse(input[i], out numbers[i]);
+
+    if (!success)
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целые числа.");
+        return;
+    }
+}
+
+int count = CountNumbersGreaterThanZero(numbers);
+Console.WriteLine("Количество чисел, больше нуля: " + count);
+
+int CountNumbersGreaterThanZero(int[] numbers)
+{
+    int count = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+/* ВТОРОЙ ВАРИАНТ ВВОДА ЧИСЕЛ
+
+int count = ReadNumbersAndCountPositive();
+Console.WriteLine("Количество чисел больше 0: " + count);
+
+int ReadNumbersAndCountPositive()
+{
+    Console.WriteLine("Введите числа через пробел (для завершения введите 'q'): ");
+    string input = Console.ReadLine();
+
+    int positiveCount = 0;
+
+    string[] numbers = input.Split(' ');
+
+    foreach (string number in numbers)
+    {
+        if (int.TryParse(number, out int parsedNumber) && parsedNumber > 0)
+        {
+            positiveCount++;
+        }
+    }
+
+    return positiveCount;
+}
+*/
+
+/* ТРЕТИЙ ВАРИАНТ ЧЕРЕЗ ВВОД КАЖДОГО ЧИСЛА "М"
+
+Console.WriteLine("Задайте, какое количество чисел вы будете вводить: ");
+int M = int.Parse(Console.ReadLine());
+int count = CountPositiveNumbers(M);
+Console.WriteLine("Количество чисел больше 0: " + count);
+
+int CountPositiveNumbers(int count)
+{
+    int positiveCount = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Введите число " + (i + 1) + ": ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            positiveCount++;
+        }
+    }
+
+    return positiveCount;
+} 
+*/
---
+++ DZ_5/Zadacha_43/Program.cs
@@ -0,0 +1,31 @@
+﻿double k1, b1, k2, b2;
+
+Console.WriteLine("Введите значения b1, k1, b2 и k2 через пробел:");
+string[] input = Console.ReadLine().Split(' ');
+
+bool success = Double.TryParse(input[0], out b1);
+success &= Double.TryParse(input[1], out k1);
+success &= Double.TryParse(input[2], out b2);
+success &= Double.TryParse(input[3], out k2);
+
+if (!success)
+{
+    Console.WriteLine("Некорректный ввод. Пожалуйста, введите числа.");
+    return;
+}
+
+double x = FindIntersectionX(k1, b1, k2, b2);
+double y = CalculateY(x, k1, b1);
+
+Console.WriteLine("Точка пересечения: (" + x + ", " + y + ")");
+
+
+double FindIntersectionX(double k1, double b1, double k2, double b2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double CalculateY(double x, double k, double b)
+{
+    return k * x + b;
+}
